449360572794cdde6e9ada24957d5416747584aa
Laguna1 <lagunazoomarket@gmail.com>
2020-09-14 18:23:57 +0300

    Is_it_balanced?.rb

---
+++ graphs.rb
@@ -0,0 +1,26 @@
+#teacher`s solution
+def graph(hash_graph)
+    # write your code here
+    next_node = 0
+    result = [0]
+    
+    while next_node != 4
+      next_node = hash_graph[next_node].first
+      result << next_node
+    end
+    
+    result
+end
+  
+  hash = { 
+    0 => [2], 
+    1 => [4], 
+    2 => [5, 0, 3], 
+    3 => [2], 
+    4 => [1, 5], 
+    5 => [4, 2]
+  }
+  
+  p graph(hash)
+  # => [0, 2, 5, 4]
+  
